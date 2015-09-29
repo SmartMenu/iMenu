410714eb83299583bbf9e71127da31259a36bc06
qingqing <xueqing227@163.com>
2015-09-29 18:53:37 +0800

    modify conflict

--- iMenu-parent/iMenu-sql/src/main/java/com/smartmenu/common/SQLEncode.java
+++ iMenu-parent/iMenu-sql/src/main/java/com/smartmenu/common/SQLEncode.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.smartmenu.common;
 
 public class SQLEncode {
@@ -7,14 +6,3 @@ public class SQLEncode {
 		return result;
 	}
 }
-=======
-package com.smartmenu.common;
-
-public class SQLEncode {
-  public static String encode(String str)
-  {
-    String result = str.replaceAll("'", "''");
-    return result;
-  }
-}
->>>>>>> 8fa8359569e10b535f6018a75b3fbd47225ff974
